fad1fae039dda6abd4130c76965221cee43786fa
rohitreddy21122000 <rohitreddy21122000@gmail.com>
2025-02-08 23:24:53 +0530

    commit2

--- src/components/Modal/modalpopup.hooks.jsx
+++ src/components/Modal/modalpopup.hooks.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 const useModalPopUp = () => {
   const [rows, setRows] = useState([
     { id: 1, col1: '598934_987_B13_03', col2: 'B13' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_01', col2: 'B11' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_02', col2: 'B12' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_03', col2: 'B13' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_04', col2: 'B14' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_05', col2: 'B15' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_06', col2: 'B16' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_07', col2: 'B17' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_08', col2: 'B18' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_09', col2: 'B19' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_10', col2: 'B10' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_11', col2: 'B11' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
-    { id: 1, col1: '598934_987_B13_12', col2: 'B12' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'}
+    { id: 2, col1: '598934_987_B13_01', col2: 'B11' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 3, col1: '598934_987_B13_02', col2: 'B12' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 4, col1: '598934_987_B13_03', col2: 'B13' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 5, col1: '598934_987_B13_04', col2: 'B14' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 6, col1: '598934_987_B13_05', col2: 'B15' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 7, col1: '598934_987_B13_06', col2: 'B16' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 8, col1: '598934_987_B13_07', col2: 'B17' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 9, col1: '598934_987_B13_08', col2: 'B18' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 10, col1: '598934_987_B13_09', col2: 'B19' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 11, col1: '598934_987_B13_10', col2: 'B10' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 12, col1: '598934_987_B13_11', col2: 'B11' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'},
+    { id: 13, col1: '598934_987_B13_12', col2: 'B12' , col3:24,col4:310,col5:0,col6:0,col7:'X7CQAP-FRO-860-VRO'}
   ]);
   const [columns, setColumns] = useState([
     { field: 'col1', headerName: 'Transmitter Name', flex: 1, editable: true ,minWidth:180},
